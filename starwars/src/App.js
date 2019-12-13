2d70390e21091092c9616fdea42974c1e49e75f6
debauchery1st <trevor.j.martin@gmail.com>
2019-12-13 12:15:51 -0500

    added axios to dependencies, api called on mount

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from "axios";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
+  const [apiState, setApiState] = useState("");
+  const swPeopleURL = "https://swapi.co/api/people/";
+  
+  const previousPage = () => apiState.previous
+  const nextPage = () => apiState.next; 
+
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  useEffect((f=setApiState) => {
+    console.log("requesting starwars information");
+    new axios.get(swPeopleURL).then((result) => {
+      const r2d2says = result.data;
+      f(r2d2says);
+    }).catch((error) => console.log);
+  }, [swPeopleURL]);
 
   return (
     <div className="App">
